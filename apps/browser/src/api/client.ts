f0a61ba31af80a83651efbfe36bbd007b5f8ffea
Moses Cosme <texanwebpro@gmail.com>
2025-09-30 17:28:54 -0500

    feat(browser): link @reactforums/core to browser app in dev

---
+++ apps/browser/src/api/client.ts
@@ -0,0 +1,5 @@
+import { ForumService } from "@reactforums/core";
+
+const baseUrl = "localhost:9999";
+
+export const forumService = new ForumService(baseUrl);
--- apps/browser/vite.config.ts
+++ apps/browser/vite.config.ts
@@ -1,17 +1,30 @@
-import { tanstackStart } from '@tanstack/react-start/plugin/vite'
-import { defineConfig } from 'vite'
-import tsConfigPaths from 'vite-tsconfig-paths'
-import viteReact from '@vitejs/plugin-react'
+import { tanstackStart } from "@tanstack/react-start/plugin/vite";
+import { defineConfig } from "vite";
+import tsConfigPaths from "vite-tsconfig-paths";
+import viteReact from "@vitejs/plugin-react";
+import path from "path";
+
+const isProd = process.env.NODE_ENV === "production";
 
 export default defineConfig({
   server: {
-    port: 3000,
+    port: 9999,
   },
   plugins: [
     tsConfigPaths({
-      projects: ['./tsconfig.json'],
+      projects: ["./tsconfig.json"],
     }),
     tanstackStart({ customViteReactPlugin: true }),
     viteReact(),
   ],
-})
+  resolve: {
+    alias: isProd
+      ? {} // use npm package in production
+      : {
+          "@reactforums/core": path.resolve(
+            __dirname,
+            "../../packages/core/src"
+          ),
+        },
+  },
+});
