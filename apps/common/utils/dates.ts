606afca611f704df93de022590c6359a2bcce067
Moses Cosme <texanwebpro@gmail.com>
2025-12-30 14:05:56 -0600

    feat(common): add date format function for stats index display

--- apps/common/utils/dates.ts
+++ apps/common/utils/dates.ts
@@ -1,9 +1,13 @@
 import { format, formatDistance } from "date-fns";
 
 export function formatDateTimeForForumDisplay(date: Date) {
-  return format(date, "MM-dd-yyyy, h:m a");
+  return format(date, "MM-dd-yyyy, h:mm a");
 }
 
 export function formatLastPostDateTime(date: Date) {
   return formatDistance(new Date(), date);
+}
+
+export function formatDateTimeForStatsDisplay(date: Date) {
+  return format(date, "PPPPp");
 }
