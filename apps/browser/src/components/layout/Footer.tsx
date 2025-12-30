4d89d63d492dd296f170d5cc4e9ad03f2cbaee78
Moses Cosme <texanwebpro@gmail.com>
2025-12-30 16:58:47 -0600

    feat(browser): add return to top function in browser app

--- apps/browser/src/components/layout/Footer.tsx
+++ apps/browser/src/components/layout/Footer.tsx
@@ -7,13 +7,23 @@ export default function Footer() {
   const homepageName = settingsService.getByName(SettingKey.HomepageName);
   const homepageUrl = settingsService.getByName(SettingKey.HomepageUrl);
 
+  const scrollToTop = () =>
+    window.scrollTo({
+      top: 0,
+      left: 0,
+      behavior: "smooth",
+    });
+
   return (
     <>
       <Edges className="w-full flex flex-col items-center justify-between gap-2 mt-4 text-sm">
         <div className="bg-stone-200 text-stone-950 font-bold p-2 w-full border-2 border-stone-300">
           <a href={homepageUrl?.value}>{String(homepageName?.value)}</a> |{" "}
-          <Link to="/">Forum Team</Link> | Return to Top | Mark all forums read
-          | RSS Syndication
+          <Link to="/team">Forum Team</Link> |{" "}
+          <span onClick={scrollToTop} className="cursor-pointer">
+            Return to Top
+          </span>{" "}
+          | Mark all forums read | RSS Syndication
         </div>
         <div className="flex flex-row items-center justify-between w-full">
           <span>Powered By reactForums, © 2025 reactForums.</span>
