c58ee502e47634d5e0851dfbae92dcf15f57f15d
Moses Cosme <texanwebpro@gmail.com>
2026-01-14 11:00:26 -0600

    feat(common): add common utils for showing reputation in ui

---
+++ apps/common/utils/reputation.ts
@@ -0,0 +1,9 @@
+export function reputationSymbol(reputation: number) {
+  if (reputation < 0) return "-";
+  return "";
+}
+
+export function reputationClassStyle(reputation: number) {
+  if (reputation === 0) return "";
+  return reputation > 0 ? "text-green-700" : "text-red-700";
+}
